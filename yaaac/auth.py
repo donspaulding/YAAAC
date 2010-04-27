36aa5bf19e9036def308608fa6e279ce47d26a83
Don Spaulding <don@box.local>
2010-04-27 08:35:06 -0500

    Added ClientLogin functionality for v2009 auth.

---
+++ yaaac/auth.py
@@ -0,0 +1,19 @@
+import urllib, urllib2
+
+CLIENT_LOGIN_URL = 'https://www.google.com/accounts/ClientLogin'
+
+def do_clientlogin(email, password, account_type, service, url=CLIENT_LOGIN_URL):
+    """
+    Submit a request to Google's ClientLogin service for installed applications.
+    """
+    data = urllib.urlencode({
+        'Email': email,
+        'Passwd': password,
+        'accountType': account_type,
+        'service': service,
+    })
+    resp = urllib2.urlopen(url, data)
+    return dict((l.split('=', 1) for l in resp.read().splitlines()))
+
+def get_adwords_auth(email, password):
+    return do_clientlogin(email, password, account_type='GOOGLE', service='adwords')['Auth']
