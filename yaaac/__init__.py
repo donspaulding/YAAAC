b15164c4bb042701efa865daaa17846442ea1003
Don Spaulding <don@box.local>
2010-07-07 18:17:07 -0500

    Updated WSDL Urls to latest adwords api WSDLs.  Bumped version.

--- yaaac/__init__.py
+++ yaaac/__init__.py
@@ -1,18 +1,21 @@
 VERSION = "0.2.1"
 SERVICE_WSDLS = {
-    "CampaignService": "https://adwords.google.com/api/adwords/cm/v201002/CampaignService?wsdl",
-    "CampaignTargetService": "https://adwords.google.com/api/adwords/cm/v201002/CampaignTargetService?wsdl",
-    "CampaignCriterionService": "https://adwords.google.com/api/adwords/cm/v201002/CampaignCriterionService?wsdl",
-    "CampaignAdExtensionService": "https://adwords.google.com/api/adwords/cm/v201002/CampaignAdExtensionService?wsdl",
-    "AdGroupAdService": "https://adwords.google.com/api/adwords/cm/v201002/AdGroupAdService?wsdl",
-    "AdGroupService": "https://adwords.google.com/api/adwords/cm/v201002/AdGroupService?wsdl",
-    "AdGroupCriterionService": "https://adwords.google.com/api/adwords/cm/v201002/AdGroupCriterionService?wsdl",
-    "AdExtensionOverrideService": "https://adwords.google.com/api/adwords/cm/v201002/AdExtensionOverrideService?wsdl",
-    "AdParamService": "https://adwords.google.com/api/adwords/cm/v201002/AdParamService?wsdl",
-    "ReportInstanceService": "https://adwords.google.com/api/adwords/cm/v201002/ReportInstanceService?wsdl",
-    "ReportDefinitionService": "https://adwords.google.com/api/adwords/cm/v201002/ReportDefinitionService?wsdl",
-    "BidLandscapeService": "https://adwords.google.com/api/adwords/cm/v201002/BidLandscapeService?wsdl",
-    "GeoLocationService": "https://adwords.google.com/api/adwords/cm/v201002/GeoLocationService?wsdl",
+    "CampaignService": "https://adwords.google.com/api/adwords/cm/v201003/CampaignService?wsdl",
+    "CampaignTargetService": "https://adwords.google.com/api/adwords/cm/v201003/CampaignTargetService?wsdl",
+    "CampaignCriterionService": "https://adwords.google.com/api/adwords/cm/v201003/CampaignCriterionService?wsdl",
+    "CampaignAdExtensionService": "https://adwords.google.com/api/adwords/cm/v201003/CampaignAdExtensionService?wsdl",
+    "AdGroupAdService": "https://adwords.google.com/api/adwords/cm/v201003/AdGroupAdService?wsdl",
+    "AdGroupService": "https://adwords.google.com/api/adwords/cm/v201003/AdGroupService?wsdl",
+    "AdGroupCriterionService": "https://adwords.google.com/api/adwords/cm/v201003/AdGroupCriterionService?wsdl",
+    "AdExtensionOverrideService": "https://adwords.google.com/api/adwords/cm/v201003/AdExtensionOverrideService?wsdl",
+    "AdParamService": "https://adwords.google.com/api/adwords/cm/v201003/AdParamService?wsdl",
+    "ReportInstanceService": "https://adwords.google.com/api/adwords/cm/v201003/ReportInstanceService?wsdl",
+    "ReportDefinitionService": "https://adwords.google.com/api/adwords/cm/v201003/ReportDefinitionService?wsdl",
+    "BidLandscapeService": "https://adwords.google.com/api/adwords/cm/v201003/BidLandscapeService?wsdl",
+    "GeoLocationService": "https://adwords.google.com/api/adwords/cm/v201003/GeoLocationService?wsdl",
+    "ReportDefinitionService": "https://adwords.google.com/api/adwords/cm/v201003/ReportDefinitionService?wsdl",
+    "MediaService": "https://adwords.google.com/api/adwords/cm/v201003/MediaService?wsdl",
+    "BidLandscapeService": "https://adwords.google.com/api/adwords/cm/v201003/BidLandscapeService?wsdl"
 }
 
 DEBUG_WSDLS = dict(
