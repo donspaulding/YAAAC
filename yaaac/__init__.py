32fab91a3d8f24032206087f90b532a9f1161a62
Don Spaulding <donspauldingii@gmail.com>
2012-03-06 16:14:55 -0600

    Migrate to v201109

--- yaaac/__init__.py
+++ yaaac/__init__.py
@@ -1,24 +1,35 @@
 SERVICE_WSDLS = {
-    "CampaignService": "https://adwords.google.com/api/adwords/cm/v201008/CampaignService?wsdl",
-    "CampaignTargetService": "https://adwords.google.com/api/adwords/cm/v201008/CampaignTargetService?wsdl",
-    "CampaignCriterionService": "https://adwords.google.com/api/adwords/cm/v201008/CampaignCriterionService?wsdl",
-    "CampaignAdExtensionService": "https://adwords.google.com/api/adwords/cm/v201008/CampaignAdExtensionService?wsdl",
-    "AdGroupAdService": "https://adwords.google.com/api/adwords/cm/v201008/AdGroupAdService?wsdl",
-    "AdGroupService": "https://adwords.google.com/api/adwords/cm/v201008/AdGroupService?wsdl",
-    "AdGroupCriterionService": "https://adwords.google.com/api/adwords/cm/v201008/AdGroupCriterionService?wsdl",
-    "AdExtensionOverrideService": "https://adwords.google.com/api/adwords/cm/v201008/AdExtensionOverrideService?wsdl",
-    "AdParamService": "https://adwords.google.com/api/adwords/cm/v201008/AdParamService?wsdl",
-    "ReportInstanceService": "https://adwords.google.com/api/adwords/cm/v201008/ReportInstanceService?wsdl",
-    "ReportDefinitionService": "https://adwords.google.com/api/adwords/cm/v201008/ReportDefinitionService?wsdl",
-    "BidLandscapeService": "https://adwords.google.com/api/adwords/cm/v201008/BidLandscapeService?wsdl",
-    "GeoLocationService": "https://adwords.google.com/api/adwords/cm/v201008/GeoLocationService?wsdl",
-    "ReportDefinitionService": "https://adwords.google.com/api/adwords/cm/v201008/ReportDefinitionService?wsdl",
-    "MediaService": "https://adwords.google.com/api/adwords/cm/v201008/MediaService?wsdl",
-    "BidLandscapeService": "https://adwords.google.com/api/adwords/cm/v201008/BidLandscapeService?wsdl"
+    "AdExtensionOverrideService": "https://adwords.google.com/api/adwords/cm/v201109/AdExtensionOverrideService?wsdl",
+    "AdGroupAdService": "https://adwords.google.com/api/adwords/cm/v201109/AdGroupAdService?wsdl",
+    "AdGroupCriterionService": "https://adwords.google.com/api/adwords/cm/v201109/AdGroupCriterionService?wsdl",
+    "AdGroupService": "https://adwords.google.com/api/adwords/cm/v201109/AdGroupService?wsdl",
+    "AdParamService": "https://adwords.google.com/api/adwords/cm/v201109/AdParamService?wsdl",
+    "BulkOpportunityService": "https://adwords.google.com/api/adwords/cm/v201109/BulkOpportunityService?wsdl",
+    "CampaignAdExtensionService": "https://adwords.google.com/api/adwords/cm/v201109/CampaignAdExtensionService?wsdl",
+    "CampaignCriterionService": "https://adwords.google.com/api/adwords/cm/v201109/CampaignCriterionService?wsdl",
+    "CampaignService": "https://adwords.google.com/api/adwords/cm/v201109/CampaignService?wsdl",
+    "CampaignTargetService": "https://adwords.google.com/api/adwords/cm/v201109/CampaignTargetService?wsdl",
+    "ConstantDataService": "https://adwords.google.com/api/adwords/cm/v201109/ConstantDataService?wsdl",
+    "ConversionTrackerService": "https://adwords.google.com/api/adwords/cm/v201109/ConversionTrackerService?wsdl",
+    "CreateAccountService": "https://adwords.google.com/api/adwords/mcm/v201109/CreateAccountService?wsdl",
+    "CustomerSyncService": "https://adwords.google.com/api/adwords/cm/v201109/CustomerSyncService?wsdl",
+    "DataService": "https://adwords.google.com/api/adwords/cm/v201109/DataService?wsdl",
+    "ExperimentService": "https://adwords.google.com/api/adwords/cm/v201109/ExperimentService?wsdl",
+    "GeoLocationService": "https://adwords.google.com/api/adwords/cm/v201109/GeoLocationService?wsdl",
+    "InfoService": "https://adwords.google.com/api/adwords/cm/v201109/InfoService?wsdl",
+    "LocationCriterionService": "https://adwords.google.com/api/adwords/cm/v201109/LocationCriterionService?wsdl",
+    "MediaService": "https://adwords.google.com/api/adwords/cm/v201109/MediaService?wsdl",
+    "MutateJobService": "https://adwords.google.com/api/adwords/cm/v201109/MutateJobService?wsdl",
+    "ReportDefinitionService": "https://adwords.google.com/api/adwords/cm/v201109/ReportDefinitionService?wsdl",
+    "ReportInstanceService": "https://adwords.google.com/api/adwords/cm/v201109/ReportInstanceService?wsdl",
+    "ServicedAccountService": "https://adwords.google.com/api/adwords/cm/v201109/ServicedAccountService?wsdl",
+    "TargetingIdeaService": "https://adwords.google.com/api/adwords/cm/v201109/TargetingIdeaService?wsdl",
+    "TrafficEstimatorService": "https://adwords.google.com/api/adwords/cm/v201109/TrafficEstimatorService?wsdl",
+    "UserListService": "https://adwords.google.com/api/adwords/cm/v201109/UserListService?wsdl",
 }
 
 DEBUG_WSDLS = dict(
-        ((k, v.replace('adwords.google.com', 'adwords-sandbox.google.com')) for k,v in SERVICE_WSDLS.items())
+        ((k, v.replace('adwords.google.com', 'adwords-sandbox.google.com')) for k, v in SERVICE_WSDLS.items())
 )
 
 from yaaac.service import AdwordsService
