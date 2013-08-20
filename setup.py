b3d5b71ec8ae9c39f425f779c41b215d0600134e
Don Spaulding <donspauldingii@gmail.com>
2013-08-20 16:24:09 -0500

    Update to v201306.  Version bump to 0.4.0.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "YAAAC",
-    version = "0.3.1",
+    version = "0.4.0",
     packages = find_packages(),
 
     # Project uses reStructuredText, so ensure that the docutils get
--- yaaac/service.py
+++ yaaac/service.py
@@ -2,9 +2,47 @@ import time
 import datetime
 
 import suds
+from suds.client import Client
 from yaaac.auth import get_adwords_auth
 
-VERSION = "0.3.1"
+VERSION = "0.4.0"
+
+
+class WebService(object):
+    def __init__(self, url):
+        self._wrapped_client = Client(url)
+        self._cached_references = {}
+
+    def __getattr__(self, name):
+        if name in self._cached_references:
+            return self._cached_references[name]
+        try:
+            new_reference = self.method_wrapper(getattr(self._wrapped_client.service, name))
+        except suds.MethodNotFound:
+            svc = self
+
+            class ServiceType(object):
+                def __new__(cls, **kwargs):
+                    obj = svc._wrapped_client.factory.create(name)
+                    for k, v in kwargs.items():
+                        setattr(obj, k, v)
+                    return obj
+            ServiceType.__name__ = name
+            new_reference = ServiceType
+        self._cached_references[name] = new_reference
+        return new_reference
+
+    def method_wrapper(self, suds_method):
+        return suds_method
+
+class NewAdwordsService(WebService):
+    def method_wrapper(self, suds_method):
+        def wrapped(*args, **kwargs):
+            headers = self.SoapHeader(
+
+            )
+            suds_method.client.set_options(
+            )
 
 
 class CallbackMethod:
@@ -36,7 +74,6 @@ class AdwordsService(suds.client.Client):
         self._developer_token = developer_token
         self._user_agent = user_agent
         self._auth_token_lifetime = auth_token_lifetime
-        self._request_delay = request_delay
         self._wrapped_methods = {}
         ###  AdWords auth tokens from the ClientLogin service are only
         ###  valid for "about one week".  The auth_token_lifetime needs
@@ -49,8 +86,6 @@ class AdwordsService(suds.client.Client):
         self._token_expirations[self._email] = datetime.datetime.now() + self._auth_token_lifetime
 
     def _set_adwords_headers(self, client_email='', client_id=''):
-        if self._request_delay is not None:
-            time.sleep(self._request_delay)
         head = self.factory.create("SoapHeader")
         head.authToken = self._token_cache[self._email]
         head.developerToken = self._developer_token
